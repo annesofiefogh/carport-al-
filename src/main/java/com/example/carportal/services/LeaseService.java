3d79a902f8b94ae8ead56e80bbc106bd6a0f18f2
Horrid Entity <etienne.olieu@gmail.com>
2022-05-18 10:13:40 +0200

    added calculate monthly earnings

--- src/main/java/com/example/carportal/services/LeaseService.java
+++ src/main/java/com/example/carportal/services/LeaseService.java
@@ -4,6 +4,7 @@ import com.example.carportal.models.Customer;
 import com.example.carportal.models.Lease;
 import com.example.carportal.repositories.LeaseRepository;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,26 @@ public class LeaseService {
 
     public void createLease(Lease lease){
         lr.create(lease);
+    }
+
+    public double calculateMonthlyEarnings(){
+        double sum = 0;
+       double price;
+
+       for (Lease l: lr.getAllOpenLeases()){
+           if (l.getStartDate().isAfter(LocalDate.of(LocalDate.now().getYear(),LocalDate.now().getMonth(),1))){
+            price = 0;
+           } else {
+            price = l.getPrice();
+           }
+               sum = price + sum;
+       }
+       return sum;
+    }
+
+    public static void main(String[] args) {
+        LeaseService ls = new LeaseService();
+        System.out.println(ls.calculateMonthlyEarnings());
     }
 
 }
